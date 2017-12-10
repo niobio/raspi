d73c0dd687fedcc3e7c409ec8e7539e3b9bb171a
niobio <jtouron@virolai.com>
2017-12-10 19:40:06 +0100

    canvis figures

---
+++ _site/README.md
@@ -0,0 +1,5 @@
+### [Introducció a la Raspberry Pi](raspberry_pi.md){:target="_blank"}
+### [Encendre un led amb Raspberry Pi](led/led.md){:target="_blank"}
+### [Sensors digitals amb la Raspberry Pi](sensors/sensor_digital.md){:target="_blank"}
+### [Sensors analògics amb la Raspberry Pi](sensors/sensor_analog.md){:target="_blank"}
+### [Sistema de reg automàtic](reg/reg.md){:target="_blank"}
---
+++ _site/sensors/sensor_analog.py
@@ -0,0 +1,45 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+
+# El pin a carrega el condensador a través d'una resistència fixa d'1k 
+# i el potenciómetre en sèrie.
+# El pin b descarrega el condensador a través de la resistència d'1k
+
+pin_a = 12
+pin_b = 16
+
+# Funció per la descàrrega del condensador, deixant-lo buit per a començar la càrrega
+def descarrega():
+    GPIO.setup(pin_a, GPIO.IN)
+    GPIO.setup(pin_b, GPIO.OUT)
+    GPIO.output(pin_b, False)
+    time.sleep(0.1)
+
+# Funció que retorna el temps de càrrega del condensador en microsegons fins arribar
+# al nivell HIGH, el qual és major o igual a 1.65V.
+def temps_carrega():
+    GPIO.setup(pin_b, GPIO.IN)
+    GPIO.setup(pin_a, GPIO.OUT)
+    GPIO.output(pin_a, True)
+    t1 = time.time()
+    while not GPIO.input(pin_b):
+        pass
+    t2 = time.time()
+    return (t2 - t1) * 1000000
+
+# Funció que pren el temps de càrrega del condensador com a lectura analògica
+# després de primer descarregar el condensador
+def lectura_analog():
+    descarrega()
+    t = temps_carrega()
+    descarrega()
+    return t
+
+try:
+    while True:
+        print(lectura_analog())
+        time.sleep(0.5)
+finally:
+    GPIO.cleanup()
