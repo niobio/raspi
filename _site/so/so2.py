c6560e07ebe36e90d37892eb401853fe5ad7b656
niobio <jtouron@virolai.com>
2017-12-11 20:43:38 +0100

    canvi en config mathjax

---
+++ _site/so/so2.py
@@ -0,0 +1,19 @@
+# Ref MCP3008: https://learn.adafruit.com/raspberry-pi-analog-to-digital-converters/mcp3008
+# -------------------------------------------------------------------
+# Recordar habilitar SPI en sudo raspi-config
+# -------------------------------------------------------------------
+import spidev, time
+
+spi = spidev.SpiDev()
+spi.open(0, 0)
+
+def analog_read(channel):
+    r = spi.xfer2([1, (8 + channel) << 4, 0])
+    adc_out = ((r[1]&3) << 8) + r[2]
+    return adc_out
+
+while True:
+    reading = analog_read(0)
+    voltage = reading * 3.3 / 1024
+    print("Reading=%d\tVoltage=%f" % (reading, voltage))
+    time.sleep(1)
